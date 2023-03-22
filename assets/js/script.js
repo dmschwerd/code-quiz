11e698434df7f28816cc7276808375165a460552
David <dmsch656@gmail.com>
2023-03-22 13:26:17 -0700

    question handling

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,38 +10,16 @@ var questions = [
         correct: "var"
     },
     {
-        question: "", 
-        answers: [],
-        correct: ""
+        question: "Which language is used to give websites more visual customization?", 
+        answers: ["CSS", "HTML", "JavaScript"],
+        correct: "CSS"
     },
     {
-        question: "What's color is the sky?", 
-        answers: ["brown", "purple", "blue"],
-        correct: "blue"
+        question: "Which terminal command stores the changes made to an existing git repository?", 
+        answers: ["git push", "git commit", "git add"],
+        correct: "git add"
     }
 ]
-
-function giveQuestion() {
-    
-    var questionEl = document.getElementById("question-area");
-    questionEl.innerHTML = "";
-
-    var questionTextEl = document.createElement('h2');
-    var questionContainerEl = document.createElement('div');
-
-    questionTextEl.textContent = questions[questionNumber].question;
-    questionContainerEl.appendChild(questionTextEl);
-
-    for(var i = 0; i < questions[questionNumber].answers.length; i++) {
-        var answerButton = document.createElement('button');
-        var answer = questions[questionNumber].answers[i];
-        answerButton.className = 'answer-button';
-        answerButton.innerHTML = answer;
-        answerButton.setAttribute('data-answer', answer);
-        questionContainerEl.appendChild(answerButton);
-    }
-    questionEl.appendChild(questionContainerEl);
-};
 
 function countdown() {
     var timerEl = document.getElementById('timer');
@@ -58,6 +36,61 @@ function countdown() {
             clearInterval(timeInterval);
         }
     }, 1000);
+};
+
+function giveQuestion() {
+    
+    var questionEl = document.getElementById("question-area");
+    questionEl.innerHTML = "";
+
+    
+    var questionTextEl = document.createElement('h2');
+    var questionContainerEl = document.createElement('div');
+    
+    if (questionNumber < questions.length) {
+        questionTextEl.textContent = questions[questionNumber].question;
+        questionContainerEl.appendChild(questionTextEl);
+    
+
+        for(var i = 0; i < questions[questionNumber].answers.length; i++) {
+            var answerButton = document.createElement('button');
+            var answer = questions[questionNumber].answers[i];
+            answerButton.className = 'answer-button';
+            answerButton.innerHTML = answer;
+            answerButton.setAttribute('data-answer', answer);
+            answerButton.addEventListener('click', function(event) {
+                evaluate(event.target.getAttribute('data-answer'))
+            });
+            questionContainerEl.appendChild(answerButton);
+        }
+        questionEl.appendChild(questionContainerEl);
+    }
+};
+
+// checks if the answer is correct
+function evaluate(answer) {
+    if(answer == questions[questionNumber].correct) {
+        correct();
+        next();
+    } else {
+        incorrect();
+        next();
+    }
+};
+
+function correct() {
+    console.log('correct');
+};
+
+function incorrect() {
+    console.log('incorrect');
+    timeLeft = timeLeft - 15;
+};
+
+function next() {
+    questionNumber++;
+    debugger;
+    giveQuestion();
 };
 
 function start() {
